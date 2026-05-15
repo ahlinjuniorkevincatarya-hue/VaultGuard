1a974637d882945e38b0c60a8a18ef35c62fa7e1
nezarelhatimi <nezarelhatimi@gmail.com>
2026-05-15 02:15:17 +0100

    feat(p2): Gestion clients & comptes - CRUD, exceptions, FormateurString

--- src/banking/exceptions/ClientInexistantException.java
+++ src/banking/exceptions/ClientInexistantException.java
@@ -1,7 +1,12 @@
 package banking.exceptions;
 
-public class ClientInexistantException extends RuntimeException {
-    public ClientInexistantException(String message) {
-        super(message);
+public class ClientInexistantException extends Exception {
+    private final String cin;
+
+    public ClientInexistantException(String cin) {
+        super("Aucun client trouvé avec le CIN : " + cin);
+        this.cin = cin;
     }
+
+    public String getCin() { return cin; }
 }
--- src/banking/model/abstracts/Compte.java
+++ src/banking/model/abstracts/Compte.java
@@ -1,4 +1,15 @@
 package banking.model.abstracts;
 
-public class Compte {
+import banking.exceptions.SoldeInsuffisantException;
+import banking.model.Client;
+
+public abstract class Compte {
+
+    public abstract String getNumeroCompte();
+    public abstract double getSolde();
+    public abstract Client getProprietaire();
+    public abstract void deposer(double montant);
+    public abstract void retirer(double montant) throws SoldeInsuffisantException;
+    public abstract String toFichier();
+    public abstract String afficher();
 }
